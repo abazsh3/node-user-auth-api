25eb742803f4c85f88bf2f91972cb84413ccdd91
abolfazl <abazsh3@gmail.com>
2019-08-03 01:14:12 +0430

    mongoose fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,8 @@
 let mongoose = require("mongoose");
 mongoose.Promise=global.Promise;
 mongoose.connect(process.env.MONOLAB_URI||"mongodb+srv://abaz:abazabaz@cluster0-tvwfm.mongodb.net/test?retryWrites=true&w=majority"
+    ,()=>{}
     ,{useNewUrlParser:true})
-    .then(res=>{
-        console.log(res);
-    })
     .catch(err=>{
         console.log(err);
     });
